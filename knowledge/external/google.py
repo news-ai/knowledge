7eb98740e760e617cfe6ffef1502a133714da648
Abhi Agarwal <me@abhiagarwal.com>
2015-12-21 15:03:11 -0500

    Adding knowledge and samples

---
+++ knowledge/external/google.py
@@ -0,0 +1,19 @@
+import json
+import os
+import urllib
+
+
+def get_google_knowledge(query):
+    api_key = os.getenv('NEWSAI_GOOGLE_KNOWLEDGE_API', '')
+    service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+    params = {
+        'query': query,
+        'limit': 10,
+        'indent': True,
+        'key': api_key,
+    }
+    url = service_url + '?' + urllib.urlencode(params)
+    response = json.loads(urllib.urlopen(url).read())
+    print response
+    for element in response['itemListElement']:
+        print element['result']['name'] + ' (' + str(element['resultScore']) + ')'
