99aae46a5fddfdf350c9a063ee968a3ad52299fb
Abhi Agarwal <me@abhiagarwal.com>
2016-02-02 16:54:23 -0500

    Moving to ES on Google Cloud

--- middleware/esclient.py
+++ middleware/esclient.py
@@ -1,14 +1,13 @@
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
-from .config import AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION
+from .config import ELASTIC_USER, ELASTIC_PASSWORD
 
-host = 'search-elastic-news-p43g5t3yo7pjesbyhois4diva4.us-west-2.es.amazonaws.com'
-awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION, 'es')
+host = 'knowledge-elastic-1.newsai.org'
 
 es = Elasticsearch(
     hosts=[{'host': host, 'port': 443}],
-    http_auth=awsauth,
+    http_auth=(ELASTIC_USER, ELASTIC_PASSWORD),
     use_ssl=True,
     verify_certs=True,
     connection_class=RequestsHttpConnection
