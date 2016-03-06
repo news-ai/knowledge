4bb5129e4413c6ce47851f19f1095dedb3c55ad7
Abhi Agarwal <me@abhiagarwal.com>
2016-03-05 21:55:40 -0500

    Removing ES

--- knowledge_server.py
+++ knowledge_server.py
@@ -1,6 +1,6 @@
 import time
 
-from middleware import log, config, esclient
+from middleware import log, config
 from knowledge import entity_extraction
 
 logger = log.setup_custom_logger('knowledge')
