572a86f60d2faba66ca3e43114ed2d71f0606ab2
Abhi Agarwal <me@abhiagarwal.com>
2016-03-18 19:05:35 -0400

    Adding sentry error checking

--- api.py
+++ api.py
@@ -5,6 +5,11 @@ from articles import process_single_article
 from context import get_login_token, get_types
 app = Flask(__name__)
 
+from raven import Client
+client = Client(
+    'https://99f7cb4fd29148f783ef5300f867570d:dabc526c069241dd852cc2b756c2cd06@app.getsentry.com/69539')
+
+
 types = {}
 
 
