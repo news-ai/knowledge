fd858fd0be5acbccdecce03ce19f07d8bb8cfdca
Abhi Agarwal <me@abhiagarwal.com>
2016-03-21 14:18:56 -0400

    Adding readability

---
+++ knowledge/external/readability.py
@@ -0,0 +1,11 @@
+from readability import ParserClient
+
+from middleware import config
+
+parser_client = ParserClient(config.READABILITY_API)
+
+
+def get_readability_url(query):
+    parser_response = parser_client.get_article(query)
+    article = parser_response.json()
+    return article
